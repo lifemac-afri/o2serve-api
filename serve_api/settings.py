42f178fbc6e10c6b17353eb3a3c117703633a637
eugene-sew <eugenesew4@gmail.com>
2024-10-04 00:59:26 +0100

    igore file added

--- serve_api/settings.py
+++ serve_api/settings.py
@@ -155,10 +155,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
